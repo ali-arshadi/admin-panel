7f00f30cafbf78e6ad900295821558470ae6d471
Ali Arshadi <realgabral@protonmail.com>
2022-12-08 17:37:37 +0330

    layouts created

---
+++ admin-panel/layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="admin-layout-wrapper">
+    <sidebar />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  components: {
+    sidebar: () => import('@/components/misc/sidebar.vue'),
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/helpers/variables';
+.admin-layout-wrapper {
+  height: 100vh;
+  background-color: $white;
+  display: flex;
+}
+</style>
