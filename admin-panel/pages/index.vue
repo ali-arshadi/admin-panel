88374faedfe3c139d57bd495979fb9d721b0ac21
Ali Arshadi <realgabral@protonmail.com>
2022-12-08 17:37:51 +0330

    adding the layour to the first page

--- admin-panel/pages/index.vue
+++ admin-panel/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial />
+  <div></div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  layout: 'admin',
+})
 </script>
