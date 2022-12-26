726ba4cf0cf1a912aea03881e4ff76d5b469ecc7
Ali Arshadi <realgabral@protonmail.com>
2022-12-26 15:41:51 +0330

    count input created

---
+++ admin-panel/components/icons/icon-minus.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="14"
+    height="3"
+    viewBox="0 0 14 3"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13 0H1C0.447812 0 0 0.447812 0 1V2C0 2.55219 0.447812 3 1 3H13C13.5522 3 14 2.55219 14 2V1C14 0.447812 13.5522 0 13 0Z"
+      fill="#8C8C8C"
+    />
+  </svg>
+</template>
---
+++ admin-panel/components/modules/inputs/count-input.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="count-input">
+    <button class="minus-button" @click="minusCount">
+      <IconMinus />
+    </button>
+    <p>{{ count }}</p>
+    <button class="plus-button" @click="plusCount">
+      <IconPlus />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  components: {
+    IconPlus: () => import('@/components/icons/icon-plus.vue'),
+    IconMinus: () => import('@/components/icons/icon-minus.vue'),
+  },
+  props: {
+    max: {
+      type: Number,
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      count: this.min,
+    };
+  },
+  methods: {
+    minusCount() {
+      if (this.count !== this.min) {
+        this.count -= 1;
+        console.log(
+          '🚀 ~ file: count-input.vue:38 ~ minusCount ~ this.count',
+          this.count
+        );
+      } else {
+        return;
+      }
+    },
+    plusCount() {
+      if (this.count !== this.max) {
+        this.count += 1;
+        console.log(
+          '🚀 ~ file: count-input.vue:49 ~ plusCount ~ this.count',
+          this.count
+        );
+      } else {
+        return;
+      }
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/helpers/variables';
+.count-input {
+  border: 2px solid $eton-blue;
+  background-color: $anti-flash-white;
+  padding: 15px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.minus-button {
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+}
+.plus-button {
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+
+  margin-left: 20px;
+}
+</style>
